d42d36fda440b994e88639ed6fd897d320c20b73
mutsukitokunaga <mutsukitokunaga@outlook.jp>
2021-01-07 07:36:50 +0000

    タスクの一覧画面、詳細画面、新規作成画面、編集画面、削除機能を作成する

--- db/migrate/20210107061434_create_tasks.rb
+++ db/migrate/20210107061434_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
       t.string :name
-      t.string :content 
+      t.string :content
 
       t.timestamps
     end
