1579c544187231d4a126572c4d46f2c83e61d99d
mutsukitokunaga <mutsukitokunaga@outlook.jp>
2021-01-08 09:17:53 +0000

    テスト実装完了

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,9 +14,9 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  config.before(:each, type: :system) do
-    driven_by(:selenium_chrome)
-  end
+  # config.before(:each, type: :system) do
+  #   driven_by(:selenium_chrome)
+  # end
   # 「headless_chromeのno-sandboxオプション」を追加する
  config.before(:each) do |example|
    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400], options: { args: ['no-sandbox'] } if example.metadata[:type] == :system
