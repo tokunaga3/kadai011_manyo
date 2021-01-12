036ea83ac33c8bcaf2d00a081ff68d4b6eb23b7e
mutsukitokunaga <mutsukitokunaga@outlook.jp>
2021-01-12 04:46:31 +0000

    作成日時の降順で並び替える、並び替えがうまくいっていることをテストする

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,36 +1,62 @@
+# require 'rails_helper'
+# RSpec.describe 'タスク管理機能', type: :system do
+#   describe '新規作成機能' do
+#     context 'タスクを新規作成した場合' do
+#       before do
+#         @task = Task.create!(name: 'test1', content: 'test2')
+#       end
+#       it '作成したタスクが表示される' do
+#         visit root_path(@task)
+#         expect(page).to have_content "test1"
+#       end
+#     end
+#   end
+#   describe '一覧表示機能' do
+#     context '一覧画面に遷移した場合' do
+#       before do
+#         @task = Task.create!(name: 'test1', content: 'test2')
+#        visit root_path
+#       end
+#       it '作成済みのタスク一覧が表示される' do
+#         expect(page).to have_content "test1"
+#       end
+#     end
+#   end
+#   describe '詳細表示機能' do
+#      context '任意のタスク詳細画面に遷移した場合' do
+#        before do
+#          @task = Task.create!(name: 'test1', content: 'test2')
+#          visit task_path(@task)
+#        end
+#        it '該当タスクの内容が表示される' do
+#          expect(page).to have_content "test1"
+#        end
+#      end
+#   end
+# end
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
-  describe '新規作成機能' do
-    context 'タスクを新規作成した場合' do
-      before do
-        @task = Task.create!(name: 'test1', content: 'test2')
-      end
-      it '作成したタスクが表示される' do
-        visit root_path(@task)
-        expect(page).to have_content "test1"
-      end
-    end
-  end
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
-      before do
-        @task = Task.create!(name: 'test1', content: 'test2')
-       visit root_path
-      end
       it '作成済みのタスク一覧が表示される' do
-        expect(page).to have_content "test1"
+        # step1で実装済みのため省略
       end
     end
   end
-  describe '詳細表示機能' do
-     context '任意のタスク詳細画面に遷移した場合' do
-       before do
-         @task = Task.create!(name: 'test1', content: 'test2')
-         visit task_path(@task)
-       end
-       it '該当タスクの内容が表示される' do
-         expect(page).to have_content "test1"
-       end
+    # テスト内容を追加で記載する
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      before do
+        Task.create!(id: 1,name: 'test1',content: 'test1')
+        Task.create!(id: 2,name: 'test2', content: 'test2')
+        # binding.irb
      end
-  end
+      it '新しいタスクが一番上に表示される' do
+        visit root_path
+        task_list = all(".task_row")
+        # ここに実装する
+        expect(task_list[0]).to have_content 'test2'
+        expect(task_list[1]).to have_content 'test1'
+      end
+    end
+
 end
