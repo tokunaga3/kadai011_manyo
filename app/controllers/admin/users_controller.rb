1fef76091d672309724348a862ee7f56e9b68125
mutsukitokunaga <mutsukitokunaga@outlook.jp>
2021-01-22 09:26:25 +0000

    ユーザを削除したら、そのユーザに紐づいているタスクを全て削除する

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,53 @@
+class Admin::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    # @user = User.find(params[:id])
+    # if @user.id == current_user.id
+    # else
+    #   redirect_to tasks_path, notice: "ログインユーザーが違うのでプロフィールはみれません"
+    # end
+  end
+
+  def edit
+    # @users.build
+    # binding.pry
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_users_path, notice: "ユーザ情報を編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to admin_users_path, notice:"ユーザを抹殺しました！"
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password,:password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
